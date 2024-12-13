2b8ad8103473622cc6b9524146718320d8c2a371
sathirak <145209193+sathirak@users.noreply.github.com>
2024-12-13 10:52:00 +0530

    init: added blogs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit" class="bg-blue-400">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import { siteTitle } from '$lib/config';
+
+	export let data;
+</script>
+
+<svelte:head>
+	<title>{siteTitle}</title>
+</svelte:head>
+
+<!-- This is the README.md file in the root of the repo. It serves double duty as the homepage's content. If you'd rather use your own HTML and/or Svelte, you can delete/modify everything in this file. -->
+<svelte:component this={data.ReadMe} />
--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,42 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-static'
+import { mdsvex } from 'mdsvex'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings'
+import rehypeSlug from 'rehype-slug'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	// Ensures both .svelte and .md files are treated as components (can be imported and used anywhere, or used as pages)
+	extensions: ['.svelte', '.md'],
+
+	preprocess: [vitePreprocess(),
+	mdsvex({
+		// The default mdsvex extension is .svx; this overrides that.
+		extensions: ['.md'],
+
+		// Adds IDs to headings, and anchor links to those IDs. Note: must stay in this order to work.
+		rehypePlugins: [
+			rehypeSlug,
+			rehypeAutolinkHeadings,
+		],
+	}),
+	],
 
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		appDir: 'app',
+		adapter: adapter(),
+		prerender: {
+			entries: [
+				'*',
+				'/api/posts/page/*',
+				'/blog/category/*/page/',
+				'/blog/category/*/page/*',
+				'/blog/category/page/',
+				'/blog/category/page/*',
+				'/blog/page/',
+				'/blog/page/*',
+			]
+		}
 	}
 };
 
